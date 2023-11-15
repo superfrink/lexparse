897d229a365e4be9805bc586d2d9c16e8cd5092b
Ian Lewis <ianmlewis@gmail.com>
2023-11-15 01:26:58 +0000

    feat: Add basic logic for parser
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

--- parser.go
+++ parser.go
@@ -15,3 +15,45 @@
 package lexparse
 
 // TODO(#459): Implement parser
+
+type Tree[V any] struct {
+	Root *Node[V]
+}
+
+type Node[V any] struct {
+	Parent   *Node[V]
+	Children []*Node[V]
+	Value    V
+}
+
+type ParseFn [V]func(chan<- *Lexeme, *Tree[V]) (ParseFn[V], error)
+
+func Parse[V any](c chan<- *Lexeme, initFn ParseFn[V]) (*Tree[V], error) {
+	// TODO:(#459): Implement Parse
+	return nil
+}
+
+// LexParse runs the Lexer passing lexemes to the parser functions.
+func LexParse[V any](l *Lexer, initFn ParseFn[V]) (*Tree[V], error) {
+	var lexErr error
+	go func() {
+		lexErr := l.Lex()
+	}()
+
+	parseFn := initFn
+	t := &Tree[V]{
+		Root: &Node[V]{},
+	}
+	var err error
+	for {
+		parseFn, err = parseFn(l.lexemes, t)
+		if err != nil {
+			// TODO: if the parser encounters an error stop the lexer
+			return t, err
+		}
+		if parseFn == nil {
+			break
+		}
+	}
+	return t, err
+}
