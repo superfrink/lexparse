c05edbbb51c5d96017e96eb9762331c3f51f2546
Ian Lewis <ianlewis@google.com>
2024-05-10 11:49:06 +0900

    test: Add template interpreter to example (#48)
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

--- example_test.go
+++ example_test.go
@@ -37,17 +37,34 @@ const (
 
 var (
 	errType   = errors.New("unexpected type")
-	errSymbol = errors.New("missing symbol")
+	errSymbol = errors.New("invalid symbol")
 )
+
+type nodeType int
+
+const (
+	textNodeType nodeType = iota
+	actionNodeType
+)
+
+type tmplNode struct {
+	typ    nodeType
+	action string
+	text   string
+}
 
 // stateText parses normal text.
 func stateText(_ context.Context, l *lexparse.Lexer) (lexparse.State, error) {
+	// Search the input for left brackets.
 	token, err := l.Find([]string{actionLeft})
+
+	// Emit the text up until this point.
 	lexeme := l.Lexeme(textType)
 	if lexeme.Value != "" {
 		l.Emit(lexeme)
 	}
 
+	// Progress to lexing the action if brackets are found.
 	var nextState lexparse.State
 	if token == actionLeft {
 		nextState = lexparse.StateFn(stateAction)
@@ -68,13 +85,15 @@ func stateAction(_ context.Context, l *lexparse.Lexer) (lexparse.State, error) {
 
 	// Find the right brackets.
 	token, err := l.Find([]string{actionRight})
-	lexeme := l.Lexeme(actionType)
-	if lexeme.Value != "" {
-		l.Emit(lexeme)
-	}
 
 	var nextState lexparse.State
 	if token == actionRight {
+		// Emit the lexeme.
+		lexeme := l.Lexeme(actionType)
+		if strings.TrimSpace(lexeme.Value) != "" {
+			l.Emit(lexeme)
+		}
+
 		// Discard the right brackets
 		if _, errDiscard := l.Discard(len(actionRight)); errDiscard != nil {
 			return nil, fmt.Errorf("lexing action: %w", errDiscard)
@@ -89,63 +108,79 @@ func stateAction(_ context.Context, l *lexparse.Lexer) (lexparse.State, error) {
 }
 
 // parseInit delegates to another parse function based on lexeme type.
-func parseInit(data map[string]string) lexparse.ParseFn[string] {
-	return func(ctx context.Context, p *lexparse.Parser[string]) (lexparse.ParseFn[string], error) {
-		l := p.Peek()
-		if l == nil {
-			return nil, nil
-		}
+func parseInit(_ context.Context, p *lexparse.Parser[*tmplNode]) (lexparse.ParseFn[*tmplNode], error) {
+	l := p.Peek()
+	if l == nil {
+		return nil, nil
+	}
 
-		switch l.Type {
-		case textType:
-			return parseText(data), nil
-		case actionType:
-			return parseAction(data), nil
-		default:
-			return nil, fmt.Errorf("%w: %v", errType, l.Type)
-		}
+	switch l.Type {
+	case textType:
+		return parseText, nil
+	case actionType:
+		return parseAction, nil
+	default:
+		return nil, fmt.Errorf("%w: %v", errType, l.Type)
 	}
 }
 
 // parseText handles normal text.
-func parseText(data map[string]string) lexparse.ParseFn[string] {
-	return func(ctx context.Context, p *lexparse.Parser[string]) (lexparse.ParseFn[string], error) {
-		l := p.Next()
-		if l == nil {
-			return nil, nil
-		}
-		p.Node(l.Value)
-		return parseInit(data), nil
+func parseText(_ context.Context, p *lexparse.Parser[*tmplNode]) (lexparse.ParseFn[*tmplNode], error) {
+	l := p.Next()
+	if l == nil {
+		return nil, nil
 	}
+	p.Node(&tmplNode{
+		typ:  textNodeType,
+		text: l.Value,
+	})
+	return parseInit, nil
 }
 
 // parseAction handles replacement actions (e.g. {{ var }}).
-func parseAction(data map[string]string) lexparse.ParseFn[string] {
-	return func(ctx context.Context, p *lexparse.Parser[string]) (lexparse.ParseFn[string], error) {
-		l := p.Next()
-		if l == nil {
-			return nil, nil
-		}
-		v, ok := data[strings.TrimSpace(l.Value)]
-		if !ok {
-			return nil, fmt.Errorf("%w: %q", errSymbol, l.Value)
-		}
-		p.Node(v)
-		return parseInit(data), nil
+func parseAction(_ context.Context, p *lexparse.Parser[*tmplNode]) (lexparse.ParseFn[*tmplNode], error) {
+	l := p.Next()
+	if l == nil {
+		return nil, nil
 	}
+
+	p.Node(&tmplNode{
+		typ:    actionNodeType,
+		action: strings.TrimSpace(l.Value),
+	})
+	return parseInit, nil
+}
+
+// execute executes the template with the given data.
+func execute(t *lexparse.Tree[*tmplNode], data map[string]string) (string, error) {
+	var b strings.Builder
+	for _, n := range t.Root.Children {
+		switch n.Value.typ {
+		case textNodeType:
+			b.WriteString(n.Value.text)
+		case actionNodeType:
+			val, ok := data[n.Value.action]
+			if !ok {
+				return b.String(), fmt.Errorf("%w: %q", errSymbol, n.Value.action)
+			}
+			b.WriteString(val)
+		}
+	}
+	return b.String(), nil
 }
 
 // ExampleLexParse implements a simple templating language.
 func ExampleLexParse() {
 	r := runeio.NewReader(strings.NewReader("Hello {{ subject }}!"))
-	data := map[string]string{"subject": "World"}
-	t, err := lexparse.LexParse(context.Background(), r, lexparse.StateFn(stateText), parseInit(data))
+	t, err := lexparse.LexParse(context.Background(), r, lexparse.StateFn(stateText), parseInit)
 	if err != nil {
 		panic(err)
 	}
-
-	for _, n := range t.Root.Children {
-		fmt.Print(n.Value)
+	txt, err := execute(t, map[string]string{"subject": "World"})
+	if err != nil {
+		panic(err)
 	}
+	fmt.Print(txt)
+
 	// Output: Hello World!
 }
